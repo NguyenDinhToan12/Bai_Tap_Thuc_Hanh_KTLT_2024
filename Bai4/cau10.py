6239fef622b3b7a794ae5ef9d4837ccd18fd3d58
NguyenDinhToan12 <dinhtoan1292005@icloud.com>
2024-12-04 11:01:09 +0700

    Add files via upload

---
+++ Bai4/cau10.py
@@ -0,0 +1,3 @@
+input_list = input("nhap mot danh sach cac phan tu (cach nhau bang dau cach): ")
+sliced_list = input_list[1:-1]
+print("danh sach sau khi cat bo phan tu dau va cuoi):", sliced_list)
