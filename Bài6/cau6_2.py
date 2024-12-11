ea1e6c95916649226f6b5db99e76dc43c68d39b0
NguyenDinhToan12 <dinhtoan1292005@icloud.com>
2024-12-11 08:50:37 +0700

    Add files via upload

---
+++ Bài6/cau6_2.py
@@ -0,0 +1,11 @@
+class HinhChunhat:
+    def __init__(self, length, width):
+        self.length = lenght
+        self.width = width
+    
+    def area(self):
+        return self.lenght * self.width
+    
+rectangle = HinhChunhat(5, 3)
+print("dien tich hinh chu nhat:", rectangle.area())
+
